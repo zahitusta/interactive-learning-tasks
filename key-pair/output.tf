208dc7b4a668fc5bff8df86fb12c8fcbebb4f517
zahitusta <zahitusta@gmail.com>
2021-08-01 05:23:06 +0000

    t2

---
+++ key-pair/output.tf
@@ -0,0 +1,11 @@
+output "key_id" {
+ value = aws_key_pair.ilearning-wordpress.id
+}
+ 
+output "key_name" {
+ value = aws_key_pair.ilearning-wordpress.key_name
+}
+ 
+output "region" {
+ value = "us-east-2"
+}
